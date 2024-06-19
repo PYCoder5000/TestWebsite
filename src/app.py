dab74a44ab20a77c6ffee7a2c5dd0ed734cec91a
PYCoder5000 <chrisbobacat@gmail.com>
2024-06-19 10:55:10 -0700

    Adjusted Website Text2

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This is a test website. Do not share it with anyone.\nThis is a test new line.\n\n\nThis is a test new stack line.'
+    return 'This is a test website. Do not share it with anyone. This is a test new line. This is a test new stack line.'
 
 
 if __name__ == '__main__':
